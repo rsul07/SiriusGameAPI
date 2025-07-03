3dcaf1b239cb710d47f714e4d27cfb977accf30d
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-07-03 12:52:57 +0300

    prod mode to hide docs

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-from fastapi import FastAPI
+import os
 from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
 from utils.migrate import create_tables, delete_tables
-
 from events.router import router as events_router
 
-class Settings():
-    debug_mode: bool = True
+ENV = os.getenv("ENV", "dev")
 
-settings = Settings()
-
-if settings.debug_mode:
+if ENV == "dev":
     @asynccontextmanager
     async def lifespan(app: FastAPI):
         await create_tables()
-        print("Tables created")
         yield
 else:
     @asynccontextmanager
     async def lifespan(app: FastAPI):
         await delete_tables()
-        print("Tables deleted")
         await create_tables()
-        print("Tables created")
         yield
 
-app = FastAPI(lifespan=lifespan, debug=True)
+app = FastAPI(
+    lifespan=lifespan,
+    debug=(ENV == "dev"),
+    docs_url=None if ENV == "prod" else "/docs",
+    redoc_url=None if ENV == "prod" else "/redoc",
+    openapi_url=None if ENV == "prod" else "/openapi.json",
+)
 
 app.include_router(events_router)
-# app.include_router(teams_router)
-# app.include_router(pushes_router)
