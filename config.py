11c718a5ad72f0145c254432c01d65f9241bd7e1
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-07-03 23:59:05 +0300

    fix: save database when rerun container

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+from pathlib import Path
 
 ENV = os.getenv("ENV", "dev")
-DB_URL = (
-    "sqlite+aiosqlite:///database_dev.db"
-    if ENV == "dev"
-    else "sqlite+aiosqlite:///database.db"
-)
+
+if ENV == "prod":
+    db_path = "/data/database.db"
+else:
+    base_dir = Path(__file__).resolve().parent
+    db_path = base_dir / "data" / "database_dev.db"
+    db_path.parent.mkdir(exist_ok=True)
+
+DB_URL = f"sqlite+aiosqlite:///{db_path}"
