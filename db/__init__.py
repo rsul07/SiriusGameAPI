6bea97f3ebe0a00e116436b4458fec277bb69f17
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-07-10 23:24:55 +0300

    fix(events): add listener for cascade deletion

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,4 @@
+from sqlalchemy import event, Engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
@@ -8,3 +9,9 @@ new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 class Model(DeclarativeBase):
     pass
+
+@event.listens_for(Engine, "connect")
+def _fk_pragma_on_connect(dbapi_conn, conn_record):
+    cursor = dbapi_conn.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
