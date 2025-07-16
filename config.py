a5d221c7ebd24ddd73df050a2248f05a87a3a88b
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-07-16 05:41:10 +0300

    feat: connect postgres using env variables

--- config.py
+++ config.py
@@ -1,19 +1,33 @@
 import os
 from pathlib import Path
 
-ENV = os.getenv("ENV", "dev")
-BASE_DIR = Path(__file__).resolve().parent
+ENV = os.getenv("ENV", "dev") # dev режим для локального запуска. на сервере всегда используется prod
+SERVER_IP = "62.183.4.195"
 
-DATA_DIR = Path("/data")
-db_path = DATA_DIR / "database.db"
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", SERVER_IP)
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "dev_db")
+
+if POSTGRES_PASSWORD is None:
+    raise ValueError("POSTGRES_PASSWORD не определен")
+
+DB_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+BASE_DIR = Path(__file__).resolve().parent
 
 SECRET_KEY = os.getenv("SECRET_KEY",
                        "yRvebEaFw2Oihv6e9MY0MZkhkvd7yhA-cG7PtSIjooFQooo7Oj2FugKYfL_39yXnr7B84Eg1r8l-EWbpkCA8Kw")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1488
 
-DB_URL = f"sqlite+aiosqlite:///{db_path}"
-MEDIA_DIR = DATA_DIR / "media"
+if ENV == "dev":
+    MEDIA_DIR = BASE_DIR / "media"
+else:
+    MEDIA_DIR = Path("media")
+
 AVATAR_DIR = MEDIA_DIR / "avatars"
 
 Path(MEDIA_DIR).mkdir(parents=True, exist_ok=True)
