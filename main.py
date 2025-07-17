0758fbd70f1c0125f2034bc07e51b61f9574970a
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-07-17 17:37:22 +0300

    style: imports at main

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from config import ENV, MEDIA_DIR
-from utils.migrate import create_tables
+
 from events.router import router as events_router
 from activities.router import events_router as event_activities_router, activities_router
 from auth.router import router as auth_router
-from config import ENV, MEDIA_DIR
-from events.router import router as events_router
 from users.router import router as users_router
 from participations.router import router as participations_router
+
+from config import ENV, MEDIA_DIR
 from utils.migrate import create_tables
-from utils.seed import create_initial_users, create_initial_events
+from utils.seed import create_initial_users
 
 
 @asynccontextmanager
